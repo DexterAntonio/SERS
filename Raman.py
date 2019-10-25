0343e0bc13b2eabbbed3e8da2eaaea00d06b69a3
Dexter Antonio <dexter.d.antonio@gmail.com>
2019-10-25 15:42:45 -0700

    replaced list with np.array for storing datavalues

--- Raman.py
+++ Raman.py
@@ -6,24 +6,41 @@ Created on Fri Oct 25 11:42:32 2019
 """
 import pandas as pd 
 import matplotlib.pyplot as plt
+import numpy as np 
 
 class Raman :
     """An class to store SERS data and analyze it """
     files_location = "C:/Users/dexte/Box/Kulkarni/Raman/data/Raman_Spectra_Clinical_Samples/Raman_Spectra_Clinical_Samples/20181115-22/" 
     def __init__(self, filename):
+        #initilize atributes 
+        self.metadata_values = {}
         self.data_dict = {}
-        self.wavelength_list = []
-        self.intensity_list= []
+        self.data_list = []
         self.filename = filename 
+        
+        #build filename strings 
+        filename_list= [self.files_location,self.filename]
+        filename_string = "".join(filename_list)
+        
+        File_object = open(filename_string,"r") #load file 
+        lines = File_object.readlines() #build iterator
+        
+        #these lists are for storing the data into dictionaries 
+        wavelength_list = []
+        intensity_list= []
+        
+        data_array = np.zeros((1024,2),dtype=float)
+        #intensity_array = np.zeros((1024,),dtype=float)
+
+        
+        on_metadata = True 
+        #this code is for processing the metadata and storing it in a dictionary 
+        run_number = 1 
         filename_list= [self.files_location,self.filename]
         filename_string = "".join(filename_list)
         File_object = open(filename_string,"r")
         lines = File_object.readlines()
-        self.metadata_values = {}
-        on_metadata = True 
-        #this code is for processing the metadata and storing it in a dictionary 
-        self.wavelength_dict = {}
-        run_number = 1 
+        i = 0 
         for line in lines:
             if(line=="\n"):
                 on_metadata = False 
@@ -54,30 +71,41 @@ class Raman :
                 continue 
             wavelength = float(splt_line[0])
             intensity = float(splt_line[1])
-            
-            if(len(self.wavelength_list)!=0 and wavelength<self.wavelength_list[-1]): #if the wavelengths go back to the start point, restart things 
-                self.data_dict[run_number] = (self.wavelength_list,self.intensity_list)
+            if(len(wavelength_list)!=0 and wavelength<wavelength_list[-1]): #if the wavelengths go back to the start point, restart things 
+                print(i)
+                self.data_dict[run_number] = (wavelength_list,intensity_list)
+                self.data_list.append(data_array)
+                data_array = np.zeros((1024,2),dtype=float)
                 run_number += 1 
-                self.wavelength_list = []
-                self.intensity_list = []
+                wavelength_list = []
+                intensity_list = []
+                i = 0 
+
                 #break 
             #[wavelength,intensity] = line.split("	")
-            self.wavelength_list.append(wavelength)
-            self.intensity_list.append(intensity)
-            
-            
+            wavelength_list.append(wavelength)
+            intensity_list.append(intensity)
+            data_array[i,0] = wavelength
+            data_array[i,1] = intensity
+            i += 1 
+
             #self.wavelength_dict[wavelength]=intensity 
-                       
-        
+                    
       
 test = Raman("20191015_SebStripe_20181115-22_trypsin_100x_dilution_on_20mM_Cysteamine_Wash_WetState_60X_4mW_SPOT1_2.txt")
-plt.figure(num=None, figsize=(20, 15), dpi=80, facecolor='w', edgecolor='k')
+plt.figure(num=2, figsize=(20, 15), dpi=80, facecolor='w', edgecolor='k')
 d = test.data_dict
-for k in d.keys():
-    xy = d[k]
-    x = xy[0]
-    y = xy[1]
-    plt.plot(x,y,'-')
+#for k in d.keys():
+i = 0 
+for d in test.data_list:
+    i += 1 
+    #xy = d[k]
+    #x = xy[0]
+    #y = xy[1]
+    #plt.plot(x,y,'-')
+    plt.plot(d[:,0],d[:,1],label=i)
+plt.legend()
+    
 
 #plt.plot(test.wavelength_list,test.intensity_list,'-')
 #for i in range(0,len())
